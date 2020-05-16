2ca7415e0889c351390e3e6d570f4ef4ed2410f2
Cindy <cindy.liu.667@gmail.com>
2020-05-16 16:45:12 -0400

    Intial commit.

---
+++ src/icsia_final/ICSIA_Final.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package icsia_final;
+
+/**
+ *
+ * @author Cindy Liu
+ */
+public class ICSIA_Final {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainForm main = new MainForm (); 
+        main.setVisible(true);
+    }
+    
+}
